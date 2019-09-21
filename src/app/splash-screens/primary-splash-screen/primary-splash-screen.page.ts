69f41948d5d47ac4eee2d1896c680d2c93d90ed6
aamirmukaram <aamirmukaram@gmail.com>
2019-09-21 13:20:53 +0500

    - Added primary splash screen

--- src/app/splash-screens/primary-splash-screen/primary-splash-screen.page.ts
+++ src/app/splash-screens/primary-splash-screen/primary-splash-screen.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-primary-splash-screen',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimarySplashScreenPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
