9869ba4e353fa425439395848ec4440c3f670611
aamirmukaram <aamirmukaram@gmail.com>
2019-09-22 23:14:43 +0500

    - Added data questions - Updated SelectLevelPage

---
+++ src/app/data/questions.ts
@@ -0,0 +1,50 @@
+/*
+levelId:
+10  = White Belt
+20  = Yellow Stripe
+30  = Yellow Belt
+40  = Green Stripe
+50  = Green Belt
+60  = Blue Stripe
+70  = Blue Belt
+80  = Red Stripe
+90  = Red Belt
+100 = Black Stripe
+110 = I DEGREE
+120 = II DEGREE
+130 = III DEGREE
+140 = IV DEGREE
+150 = V DEGREE
+160 = VI DEGREE
+ */
+
+/*
+questionCategoryId:
+10  = Vocabulary
+20  = Patterns
+30  = Qualities of a Good Instructor
+*/
+
+/*
+questionTypeId:
+10  = MCQs
+*/
+
+
+export const questionsList = [
+  {
+    levelId: 10,
+    questions: [
+      {
+        questionCategoryId: 10,
+        questionCategoryDisabled: false,
+        questionTypeId: 10,
+        question: {
+          text: 'This is a MCQs Question',
+          options: ['A', 'B', 'C', 'D'],
+          answer: 'A'
+        }
+      }
+    ]
+  }
+];
